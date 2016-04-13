ebee11f2d61865dfb6a25e6c72c4318a725167f2
burtchen <burtchen@web.de>
2016-04-13 18:26:22 +0200

    rename variables in color difference calculation

--- js/ColorDifference.js
+++ js/ColorDifference.js
@@ -1,15 +1,15 @@
 export default function colorDifference(comparedColor, referenceColor) {
-    const deltaL = referenceColor.lightness - comparedColor.lightness;
+    const deltaLightness = referenceColor.lightness - comparedColor.lightness;
     const deltaA = referenceColor.a - comparedColor.a;
     const deltaB = referenceColor.b - comparedColor.b;
 
-    //const deltaE = Math.sqrt(Math.pow(deltaL, 2) + Math.pow(deltaA, 2) + Math.pow(deltaB, 2));
+    //const deltaE = Math.sqrt(Math.pow(deltaLightness, 2) + Math.pow(deltaA, 2) + Math.pow(deltaB, 2));
 
     const referenceChroma = Math.sqrt(Math.pow(referenceColor.a, 2) + Math.pow(referenceColor.b, 2));
     const compareChroma = Math.sqrt(Math.pow(comparedColor.a, 2) + Math.pow(comparedColor.b, 2));
-    const deltaC = referenceChroma - compareChroma;
-    const deltaHue = Math.sqrt(Math.pow(deltaA, 2) + Math.pow(deltaB, 2) + Math.pow(deltaC, 2));
+    const deltaChroma = referenceChroma - compareChroma;
+    const deltaHue = Math.sqrt(Math.pow(deltaA, 2) + Math.pow(deltaB, 2) + Math.pow(deltaChroma, 2));
 
-    const deltaE94 = Math.sqrt(Math.pow(deltaL, 2) + Math.pow(deltaC / (1 + (0.045 * referenceChroma)), 2) + Math.pow(deltaHue / (1 + (0.015 * referenceChroma)), 2));
+    const deltaE94 = Math.sqrt(Math.pow(deltaLightness, 2) + Math.pow(deltaChroma / (1 + (0.045 * referenceChroma)), 2) + Math.pow(deltaHue / (1 + (0.015 * referenceChroma)), 2));
     return Math.round(deltaE94, 1);
 }
